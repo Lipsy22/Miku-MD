698a2b0859771b8ffdbb8a3afae2622cac6d4ae8
Lipsy22 <118037390+Lipsy22@users.noreply.github.com>
2022-11-20 23:23:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['918101187835']
-global.OwnerNumber = ['918101187835']
-global.ownertag = ['918101187835']
-global.BotName = "Miku"
-global.packname = "Miku bot"
-global.author = "By: Fantox"
-global.OwnerName = "Fantox"
+global.Owner = ['919747145506']
+global.OwnerNumber = ['919747145506']
+global.ownertag = ['919747145506']
+global.BotName = "abhiiiee"
+global.packname = "abhiiiee"
+global.author = "By: abxiie"
+global.OwnerName = "abxiie"
 global.BotSourceCode = "https://github.com/FantoX001/Miku-MD"
 global.SupportGroupLink = "https://chat.whatsapp.com/JcBzjRIxM5UHfuJbt0KrWt"
 global.sessionName = "session"
